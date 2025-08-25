d7469d5bc45cf237436a5c5ddcf778bea70e4fa0
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2025-08-26 00:32:32 +0600

    core

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.FRONTENDURL || 'https://ecommerce-dashboard-client-black.vercel.app',
+    origin: process.env.FRONTENDURL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', "OPTIONS"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
