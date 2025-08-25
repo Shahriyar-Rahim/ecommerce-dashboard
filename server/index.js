b1daca14cda275841bd649ab4399722ef889fef9
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2025-08-26 01:04:35 +0600

    core

--- server/index.js
+++ server/index.js
@@ -13,13 +13,14 @@ const cache = new NodeCache({ stdTTL: 600 });
 app.use(express.json());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.FRONTENDURL || 'http://localhost:5173' || 'https://ecommerce-dashboard-virid.vercel.app/api/dashboard/analytics' || 'https://ecommerce-dashboard-client-black.vercel.app',
-    accessControlAllowOrigin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', "OPTIONS"],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: process.env.FRONTENDURL || 'http://localhost:5173' || 'https://ecommerce-dashboard-virid.vercel.app/api/dashboard/analytics' || 'https://ecommerce-dashboard-client-black.vercel.app',
+//     accessControlAllowOrigin: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', "OPTIONS"],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 
 const uri = process.env.MONGODB_URL;
